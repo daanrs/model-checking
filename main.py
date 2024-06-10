e34c3e55a7f1be02a5c0c2bd0ee60186438a3782
daanrs <git@daan.rs>
2024-06-10 12:47:07 +0200

    main: fixes

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import stormpy.examples.files
 import pycarl
 
 def create_uMdp_matrix(mdp, epsilon):
-    builder = stormpy.IntervalSparseMatrixBuilder(rows=0, columns=0, entries=0, force_dimensions=False, has_custom_row_grouping=True, row_groups=14)
+    builder = stormpy.IntervalSparseMatrixBuilder(rows=0, columns=0, entries=0, force_dimensions=False, has_custom_row_grouping=True, row_groups=0)
 
     nr_actions = 0
     for (i, state) in enumerate(mdp.states):
-        print(i)
         builder.new_row_group(nr_actions)
 
         for (act, action) in enumerate(state.actions):
             for transition in action.transitions:
                 val = transition.value()
                 act = action.id + nr_actions
-                print(f'{act}, {transition.column}, {val}')
                 if val == 1:
                     builder.add_next_value(act, transition.column, pycarl.Interval(1, 1))
                 else:
@@ -30,7 +28,7 @@ maze = stormpy.examples.files.prism_mdp_maze
 maze_model = stormpy.parse_prism_program(maze)
 maze_final = stormpy.build_model(maze_model)
 
-print(maze_final)
+# print(maze_final)
 
 m = create_uMdp_matrix(maze_final, 0.2)
 print(m)
