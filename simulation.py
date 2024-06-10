3e60561ba1fd9d446a561537d86e8c8a14f24979
Robert <romi@scrooge>
2024-06-10 11:46:23 +0200

    simulate returns probabilities per state-action pair

--- simulation.py
+++ simulation.py
@@ -3,6 +3,7 @@ import stormpy.examples
 import stormpy.examples.files
 import stormpy.simulator
 import random
+from collections import Counter
 
 random.seed(23)
 path = stormpy.examples.files.prism_mdp_slipgrid
@@ -12,20 +13,23 @@ model = stormpy.build_model(prism_program)
 simulator = stormpy.simulator.create_simulator(model, seed=42)
 
 def simulate(num_paths = 30, max_path_length = 20):
-    paths = []
+    frequencies = Counter() # (state_from, action, state_to) -> frequency
+    total_frequencies = Counter() # (state, action) -> frequency
     for m in range(num_paths):
-        path = []
-        state, reward, labels = simulator.restart()
-        path = [f"{state}"]
+        state_from, reward, labels = simulator.restart()
         for n in range(max_path_length):
             actions = simulator.available_actions()
             select_action = random.randint(0,len(actions)-1)
-            path.append(f"{actions[select_action]}")
-            state, reward, labels = simulator.step(actions[select_action])
-            path.append(f"{state}")
+            state_to, reward, labels = simulator.step(actions[select_action])
+            frequencies[(state_from, actions[select_action], state_to)] += 1
+            total_frequencies[(state_from, actions[select_action])] += 1
             if simulator.is_done():
                 break
-        paths.append(path)
-    return paths
+            state_from = state_to
+    
+    for key in frequencies:
+        frequencies[key] /= total_frequencies[(key[0], key[1])] # turn freuqencies into probabilities
+    return frequencies
 
-print(simulate())
+frequencies = simulate()
+print(frequencies)
