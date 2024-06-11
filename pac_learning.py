559e3501eebe6e5a2af863bdb375aab7d64c0f3d
Robert <romi@scrooge>
2024-06-11 11:27:50 +0200

    PAC learning uses frequentist estimates

--- pac_learning.py
+++ pac_learning.py
@@ -1,38 +1,38 @@
 import stormpy
-import stormpy.examples
-import stormpy.examples.files
 import pycarl
 import math
 
-# first perform frequentist learning
-n_1 = 5
-n_2 = 8
-n = n_1 + n_2
+from frequentist_learning import frequentist_learning
+from simulation import Measurement
+from main import update_interval_mdp
 
-error_rate = 0.1
-m = 1 # number of successor states with probabilities in (0,1)
-epsilon_M = error_rate / m
-delta_M = math.sqrt(math.log(2 / epsilon_M) / (2 * n))
+def pac_learning(model, frequencies, error_rate = 0.1):
+    # first perform frequentist learning
+    frequentist_estimates = frequentist_learning(frequencies.probabilities())
 
-builder = stormpy.IntervalSparseMatrixBuilder(rows=0, columns=0, entries=0, force_dimensions=False, has_custom_row_grouping=True, row_groups=0)
-builder.new_row_group(0)
-builder.add_next_value(0, 1, pycarl.core.Interval(n_1 / n - delta_M, n_1 / n + delta_M)) # +- delta_M
-builder.add_next_value(0, 2, pycarl.core.Interval(n_2 / n - delta_M, n_2 / n + delta_M)) # +- delta_M
-builder.new_row_group(1)
-builder.add_next_value(1, 1, pycarl.core.Interval(1, 1))
-builder.new_row_group(2)
-builder.add_next_value(2, 2, pycarl.core.Interval(1, 1))
+    # compute error bounds
+    m = 0 # number of successor states with probabilities in (0,1)
+    for state in model.states:
+        for action in state.actions:
+            for transition in action.transitions:
+                if 0 < transition.value() < 1:
+                    m += 1
+    epsilon_M = error_rate / m # stays the same for every (state, action) pair
 
+    builder = stormpy.IntervalSparseMatrixBuilder(rows=0, columns=0, entries=0, force_dimensions=False, has_custom_row_grouping=True, row_groups=0)
+    number_actions = 0
+    for state in model.states:
+        builder.new_row_group(number_actions)
+        for action in state.actions:
+            N = frequencies.total_frequencies[state.id, action.id]
+            delta_M = math.sqrt(math.log(2 / epsilon_M) / (2 * N)) # different for every (state, action) pair
 
-transition_matrix = builder.build()
-
-state_labeling = stormpy.storage.StateLabeling(3)
-labels = {'init', 'one', 'two'}
-for label in labels:
-    state_labeling.add_label(label)
-
-
-
-components = stormpy.SparseIntervalModelComponents(transition_matrix=transition_matrix, state_labeling=state_labeling)
-mdp = stormpy.storage.SparseIntervalMdp(components)
-print(mdp)
+            act = action.id + number_actions
+            for transition in action.transitions:
+                estimate = frequentist_estimates[state.id, act, transition.column]
+                pac_interval = pycarl.Interval(max(0, estimate - delta_M), min(1, estimate + delta_M))
+                builder.add_next_value(act, transition.column, pac_interval)
+        number_actions += len(state.actions)
+    
+    transition_matrix = builder.build()
+    return update_interval_mdp(model=model, new_matrix=transition_matrix)
