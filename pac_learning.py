791d690d769d6f7005fb9d832d3d6b7fb50c895a
Robert <romi@scrooge>
2024-06-12 10:44:22 +0200

    pac_learning: keep previous value if state-action-pair has no estimate

--- pac_learning.py
+++ pac_learning.py
@@ -8,10 +8,7 @@ def pac(model, measurement, error_rate = 0.1):
     matrix = pac_create_matrix(model, measurement, error_rate)
     return update_interval_mdp(model, matrix)
 
-def pac_create_matrix(model, frequencies, error_rate = 0.1):
-    # probability estimates from frequentist learning
-    probability_estimates = frequencies.probabilities()
-
+def pac_create_matrix(model, measurement, error_rate = 0.1):
     # compute error bounds
     m = 0 # number of successor states with probabilities in (0,1)
     for state in model.states:
@@ -26,20 +23,20 @@ def pac_create_matrix(model, frequencies, error_rate = 0.1):
     for state in model.states:
         builder.new_row_group(number_actions)
         for action in state.actions:
-            N = frequencies.total_frequencies[state.id, action.id]
+            N = measurement.get_total_frequency(state.id, action.id)
             delta_M = math.sqrt(math.log(2 / epsilon_M) / (2 * N)) # different for every (state, action) pair
 
             act = action.id + number_actions
-            for transition in action.transitions:
-                key = (state.id, action.id, transition.column)
-                if key in probability_estimates:
-                    # update probability estimate with PAC interval
-                    estimate = probability_estimates[key]
+            if (measurement.has_estimate(state.id, action.id)):
+                # update probability estimate with PAC interval
+                for transition in action.transitions:
+                    estimate = measurement.get_probability(state.id, action.id, transition.column)
                     pac_interval = pycarl.Interval(max(0, estimate - delta_M), min(1, estimate + delta_M))
                     builder.add_next_value(act, transition.column, pac_interval)
-                else:
-                    # no probability estimate available, keep previous probability interval
-                    builder.add_next_value(act, transition.column, transition.value())
+            else:
+                # no probability estimate available, keep previous probability interval
+                for transition in action.transitions:
+                    builder.add_next_value(act, transition.column, transition.value)
         number_actions += len(state.actions)
     
     transition_matrix = builder.build()
