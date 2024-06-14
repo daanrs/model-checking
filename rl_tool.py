25483fdc8e01e4da13d7b79707ed83b5ba0ea856
Robert <romi@scrooge>
2024-06-14 14:17:29 +0200

    rl_tool: accepts algorithm as input

--- rl_tool.py
+++ rl_tool.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
                         help='path to a file containing a PRISM model')
     parser.add_argument('target_label', metavar='target_label', type=str, nargs=1,
                         help='target label in the PRISM model')
+    parser.add_argument('--algorithm', metavar='algorithm', type=str, nargs=1,
+                        help='algorithm to run from: map, frequentist, lui, pac, ucrl2\\default: all', default='all')
 
     args = parser.parse_args()
 
@@ -25,12 +27,24 @@ if __name__ == "__main__":
     model = stormpy.build_model(program, properties)
     rewards = state_action_rewards_from_model(model)
 
-    df = {
-        "map": main_map(model, paths_per_run, formula=formula, gamma = gamma, rewards=rewards),
-        "frequentist": main_frequentist(model, paths_per_run, formula=formula, gamma = gamma, rewards=rewards),
-        "lui": main_lui(model, paths_per_run=paths_per_run, formula=formula, gamma = gamma, rewards=rewards),
-        "pac": main_pac(model, paths_per_run=paths_per_run, formula=formula, gamma = gamma, rewards=rewards),
-        "ucrl2": main_ucrl2(model, loops=15, formula=formula, gamma=gamma, rewards=rewards)
-    }
+    algorithm = args.algorithm[0]
+    if algorithm == "map":
+        df = {"map": main_map(model, paths_per_run, formula=formula, gamma = gamma, rewards=rewards)}
+    elif algorithm == "frequentist":
+        df = {"frequentist": main_frequentist(model, paths_per_run, formula=formula, gamma = gamma, rewards=rewards)}
+    elif algorithm == "lui":
+        df = {"lui": main_lui(model, paths_per_run=paths_per_run, formula=formula, gamma = gamma, rewards=rewards)}
+    elif algorithm == "pac":
+        df = {"pac": main_pac(model, paths_per_run=paths_per_run, formula=formula, gamma = gamma, rewards=rewards)}
+    elif algorithm == "ucrl2":
+        df = {"ucrl2": main_ucrl2(model, loops=15, formula=formula, gamma=gamma, rewards=rewards)}
+    else:
+        df = {
+            "map": main_map(model, paths_per_run, formula=formula, gamma = gamma, rewards=rewards),
+            "frequentist": main_frequentist(model, paths_per_run, formula=formula, gamma = gamma, rewards=rewards),
+            "lui": main_lui(model, paths_per_run=paths_per_run, formula=formula, gamma = gamma, rewards=rewards),
+            "pac": main_pac(model, paths_per_run=paths_per_run, formula=formula, gamma = gamma, rewards=rewards),
+            "ucrl2": main_ucrl2(model, loops=15, formula=formula, gamma=gamma, rewards=rewards)
+        }
 
     json.dump(df, sys.stdout)
